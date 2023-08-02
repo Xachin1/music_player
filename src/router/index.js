f9d1e34e01ee81a0c3491807289c89af49194511
Xachin1 <sbhandary123@gmail.com>
2023-08-02 22:57:43 +1000

    routing completed

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,48 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+import Manage from '@/views/Manage.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/AboutView',
+    name: 'about',
+    component: AboutView
+  },
+  {
+    path: '/manage-music',
+    name: 'manage',
+    component: Manage,
+    beforeEnter: (to, from, next) => {
+      console.log('Manage Router guard')
+      next()
+    }
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' }
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' }
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
+})
+
+router.beforeEach((to, from, next) => {
+  console.log('Global Guard')
+
+  next()
 })
 
 export default router
